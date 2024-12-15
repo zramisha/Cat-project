86b38f23d0ac20e1796eb4f640a467b060b35679
Zubaida <hzubaida66@gmail.com>
2024-12-16 04:10:29 +0600

    cat crud operation added

--- cat-backend/routes/index.js
+++ cat-backend/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 import authRoutes from "./authRoutes.js";
+import catRoutes from "./catRoutes.js"
 
 
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 const path = "/api/v1/";
 
 router.use(`${path}auth`, authRoutes);
+router.use(`${path}cats`, catRoutes);
 
 export default router;
