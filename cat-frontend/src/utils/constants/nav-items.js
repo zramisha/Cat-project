56a2766ebe6b814ba6ed74d15fdc95f9fc8127f8
TNVR-HSN <tanveer.hasan147@gmail.com>
2025-01-05 17:23:59 +0600

    Frontend: Navbar updated

--- cat-frontend/src/utils/constants/nav-items.js
+++ cat-frontend/src/utils/constants/nav-items.js
@@ -17,7 +17,8 @@ export const navItems = [
     { name: "Profile", link: routesConstant.profile,  },
     { name: "Explore Cats", link: routesConstant.exploreCats, },
     {name: "Cat Posts", link: routesConstant.catPosts},
-    { name: "Wishlist", link: routesConstant.wishList, },
-    { name: "Setting", link: "/",  },
+    {name: "Shelters", link: routesConstant.shelters},
+    {name: "Events", link: routesConstant.events},
+    { name: "Wishlist", link: routesConstant.profile, },
 
   ];
